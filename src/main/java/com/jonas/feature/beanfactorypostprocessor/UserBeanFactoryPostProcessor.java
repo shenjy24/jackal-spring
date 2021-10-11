5866f2ecf4e5f5cb6fd3219807e77b790055b35c
shenjy <shenjy24@gmail.com>
2021-10-11 17:20:14 +0800

    BeanFactoryPostProcessor

---
+++ src/main/java/com/jonas/feature/beanfactorypostprocessor/UserBeanFactoryPostProcessor.java
@@ -0,0 +1,24 @@
+package com.jonas.feature.beanfactorypostprocessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * BeanFactoryPostProcessor是在spring容器加载了bean的定义文件之后，在bean实例化之前执行的。
+ */
+public class UserBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BeanDefinition bd = beanFactory.getBeanDefinition("userBean");
+        MutablePropertyValues pv =  bd.getPropertyValues();
+        if (pv.contains("userName")) {
+            pv.addPropertyValue("userName", "Tom");
+        }
+        bd.setScope(BeanDefinition.SCOPE_SINGLETON);
+    }
+
+}
